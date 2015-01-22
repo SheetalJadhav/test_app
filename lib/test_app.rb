f7a96ec8d1496c06340d405ee50b9dec1a5e09c9
Sheetal Jadhav <sheetal.jadhav@izeltech.com>
2015-01-22 13:07:23 +0530

    testing rdoc

--- lib/test_app.rb
+++ lib/test_app.rb
@@ -2,4 +2,9 @@ require "test_app/version"
 
 module TestApp
   # Your code goes here...
+  # Creates MT safe Client instance that manages rest api calls to Bitcasa Cloud
+		# @param clientid [String] application clientid
+		# @param secret [String] application secret
+		# @param host [String] server address
+		# @return Client class instance
 end
